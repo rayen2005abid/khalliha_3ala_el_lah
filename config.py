e726c6166b17b3ea592e316f637f0fa18a7a6d35
Rayen_Abid_2025 <156456695+rayen2005abid@users.noreply.github.com>
2025-04-20 06:03:44 +0100

    Create config.py

---
+++ config.py
@@ -0,0 +1,8 @@
+
+# Wi-Fi Configuration
+WIFI_SSID = 'your_wifi_ssid'
+WIFI_PASSWORD = 'your_wifi_password'
+
+# Server Configuration
+SERVER_IP = '192.168.1.100'  # Replace with your PC's IP
+SERVER_PORT = 8080
